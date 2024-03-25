957b51f7540ecb235a5619cbd3d8e2b017b76e6b
José Á. Calero M. <joseacalerom@gmail.com>
2024-03-25 21:25:32 +0100

    PATCH fixed

--- src/routes/books.routes.js
+++ src/routes/books.routes.js
@@ -1,146 +1,156 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const Book = require('../models/books.model');
+const Book = require("../models/books.model");
 
 //MIDDLEWARE
 const getBook = async (req, res, next) => {
-    let book;
-    const { id } = req.params;
+  let book;
+  const { id } = req.params;
 
-    if(!id.match(/^[0-9a-fA-F]{24}$/)){
-        return res.status(404).json({
-            message: 'El ID del libro no es válido'
-        })
+  if (!id.match(/^[0-9a-fA-F]{24}$/)) {
+    return res.status(404).json({
+      message: "El ID del libro no es válido",
+    });
+  }
+
+  try {
+    book = await Book.findById(id);
+    if (!book) {
+      return res.status(404).json({
+        message: "El libro no fue encontrado",
+      });
     }
+  } catch (error) {
+    return res.status(500).json({
+      message: error.message,
+    });
+  }
 
-    try{
-        book = await Book.findById(id);
-        if(!book){
-            return res.status(404).json({
-                message: 'El libro no fue encontrado'
-            })
-        }
-
-    }catch(error){
-        return res.status(500).json({
-            message: error.message
-        })
-    }
-
-    res.book = book;
-    next();
-}
+  res.book = book;
+  next();
+};
 
 //Obtener todos los libros [GET ALL]
-router.get('/', async (req, res) => {
-    try{
-        const books = await Book.find();
-        console.log('Get all: ', books);
-        if(books.length === 0){
-            return res.status(204).json([]);
-        }
-        res.json(books);
-    }catch(error){
-        res.status(500).json({ message: error.message })
+router.get("/", async (req, res) => {
+  try {
+    const books = await Book.find();
+    console.log("Get all: ", books);
+    if (books.length === 0) {
+      return res.status(204).json([]);
     }
+    res.json(books);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 });
 
 //Crear nuevo libro (reurso) [POST]
 
-router.post('/', async (req, res) => {
-    const { title, author, genre, publication_date, original_language } = req?.body;
-    if(!title || !author || !genre || !publication_date || !original_language){
-        return res.status(400).json({
-            message: 'Los campos \'title\', \'author\', \'genre\', \'publication_date\' y \'original_language\' son obligatorios.'
-        });
-    }
+router.post("/", async (req, res) => {
+  const { title, author, genre, publication_date, original_language } =
+    req?.body;
+  if (!title || !author || !genre || !publication_date || !original_language) {
+    return res.status(400).json({
+      message:
+        "Los campos 'title', 'author', 'genre', 'publication_date' y 'original_language' son obligatorios.",
+    });
+  }
 
-    const book = new Book({
-        title,
-        author,
-        genre,
-        publication_date,
-        original_language
-    })
+  const book = new Book({
+    title,
+    author,
+    genre,
+    publication_date,
+    original_language,
+  });
 
-    try{
-        const newBook = await book.save();
-        console.log(newBook);
-        res.status(201).json(newBook);
-    }catch(error){
-        res.status(400).json({
-            message: error.message
-        })
-    }
+  try {
+    const newBook = await book.save();
+    console.log(newBook);
+    res.status(201).json(newBook);
+  } catch (error) {
+    res.status(400).json({
+      message: error.message,
+    });
+  }
 });
 
 //Obtener un libro individual
-router.get('/:id', getBook, async (req, res) => {
-    res.json(res.book);
+router.get("/:id", getBook, async (req, res) => {
+  res.json(res.book);
 });
 
 //Método PUT
-router.put('/:id', getBook, async (req, res) => {
-    try{
-        const book = res.book;
-        book.title = req.body.title || book.title;
-        book.author = req.body.author || book.author;
-        book.genre = req.body.genre || book.genre;
-        book.publication_date = req.body.publication_date || book.publication_date;
-        book.original_language = req.body.original_language || book.original_language;
+router.put("/:id", getBook, async (req, res) => {
+  try {
+    const book = res.book;
+    book.title = req.body.title || book.title;
+    book.author = req.body.author || book.author;
+    book.genre = req.body.genre || book.genre;
+    book.publication_date = req.body.publication_date || book.publication_date;
+    book.original_language =
+      req.body.original_language || book.original_language;
 
-        const updatedBook = await book.save();
-        res.json(updatedBook);
-
-    }catch(error){
-        res.status(400).json({
-            message: error.message
-        })
-    }
+    const updatedBook = await book.save();
+    res.json(updatedBook);
+  } catch (error) {
+    res.status(400).json({
+      message: error.message,
+    });
+  }
 });
 
 //Método PATCH
-router.patch('/:id', getBook, async (req, res) => {
+router.patch("/:id", getBook, async (req, res) => {
+  if (!req.body.title && !req.body.author && !req.body.genre && !req.body.publication_date && !req.body.original_language) {
+    return res.status(400).json({
+      message:
+        "Al menos uno de los campos ('title', 'author', 'genre', 'publication_date' y 'original_language') es obligatorio.",
+    });
+  }
 
-    if(!title || !author || !genre || !publication_date || !original_language){
-        return res.status(400).json({
-            message: 'Al menos uno de los campos (\'title\', \'author\', \'genre\', \'publication_date\' y \'original_language\') es obligatorio.'
-        });
-    }
+  if (req.body.title != null) {
+    res.book.title = req.body.title;
+  }
 
-    try{
-        const book = res.book;
-        book.title = req.body.title || book.title;
-        book.author = req.body.author || book.author;
-        book.genre = req.body.genre || book.genre;
-        book.publication_date = req.body.publication_date || book.publication_date;
-        book.original_language = req.body.original_language || book.original_language;
+  if (req.body.author != null) {
+    res.book.author = req.body.author;
+  }
+  if (req.body.genre != null) {
+    res.book.genre = req.body.genre;
+  }
+  if (req.body.publication_date != null) {
+    res.book.publication_date = req.body.publication_date;
+  }
+  if (req.body.original_language != null) {
+    res.book.original_language = req.body.original_language;
+  }
 
-        const updatedBook = await book.save();
-        res.json(updatedBook);
-
-    }catch(error){
-        res.status(400).json({
-            message: error.message
-        })
-    }
+  try {
+    const updatedBook = await res.book.save();
+    res.json(updatedBook);
+  } catch (error) {
+    res.status(400).json({
+      message: error.message,
+    });
+  }
 });
 
 //Método DELETE
-router.delete('/:id', getBook, async (req, res) => {
-    try{
-        const book = res.book;
-        await book.deleteOne({
-            _id: book._id
-        });
-        res.json({
-            message: `El libro ${book.title} ha sido eliminado`
-        })
-    }catch(error){
-        res.status(500).json({
-            message: error.message
-        })
-    }
+router.delete("/:id", getBook, async (req, res) => {
+  try {
+    const book = res.book;
+    await book.deleteOne({
+      _id: book._id,
+    });
+    res.json({
+      message: `El libro ${book.title} ha sido eliminado`,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
 });
 
 module.exports = router;
